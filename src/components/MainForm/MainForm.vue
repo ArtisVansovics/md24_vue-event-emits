e23183dcf7dd26525c30bfb59390b08b8797d24f
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-05 20:58:05 +0300

    Created MainForm component

---
+++ src/components/MainForm/MainForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form class="form" v-on:submit.prevent="submitHandler">
+    <slot></slot>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainForm",
+  emits: ["onSubmit"],
+  methods: {
+    submitHandler() {
+      this.$emit("onSubmit");
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss" src="./MainForm.scss" />
