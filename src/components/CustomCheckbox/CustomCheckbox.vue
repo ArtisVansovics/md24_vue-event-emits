16fa7fdd3ca11354d3356a4fce3cb3d383bb20e6
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-06 01:19:54 +0300

    Created CustomCheckbox

---
+++ src/components/CustomCheckbox/CustomCheckbox.vue
@@ -0,0 +1,14 @@
+<template>
+  <input :class="['checkbox', { checked: isChecked }]" type="checkbox" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CustomCheckbox",
+  props: ["isChecked"],
+});
+</script>
+
+<style scoped lang="scss" src="./CustomCheckbox.scss" />
