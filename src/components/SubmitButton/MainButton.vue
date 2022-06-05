3f2d90d81a45cd64e75232c25143e25e6a3ff25a
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-05 18:52:51 +0300

    Created MainButton component

---
+++ src/components/SubmitButton/MainButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="button" @click="clickHandler">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainButton",
+  emits: ["onClick"],
+  methods: {
+    clickHandler() {
+      this.$emit("onClick");
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss" src="./MainButton.scss" />
