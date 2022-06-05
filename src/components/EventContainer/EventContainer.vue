94d35af7d213bb5d0f519f8e08c745292cb12e41
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-05 20:59:05 +0300

    Created EventContainer component with slots for other components

---
+++ src/components/EventContainer/EventContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="event-container">
+    <slot name="header"></slot>
+    <div class="row">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "EventContainer",
+});
+</script>
+
+<style scoped lang="scss" src="./EventContainer.scss" />
