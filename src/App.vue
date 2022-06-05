e57ef221ff4fb30b4e9c5d60d6efa6f9ed460e0d
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-06 01:20:43 +0300

    Created VisibilityBox02, in which square visibility can be toggled with checkbox

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
         <DescriptionBox01 />
         <DescriptionBox02 />
         <VisibilityBox01 />
+        <VisibilityBox02 />
       </div>
     </div>
   </div>
@@ -25,10 +26,12 @@ import ColorBox02 from "@/components/ColorBox02.vue";
 import DescriptionBox01 from "@/components/DescriptionBox01.vue";
 import DescriptionBox02 from "@/components/DescriptionBox02.vue";
 import VisibilityBox01 from "@/components/VisibilityBox01.vue";
+import VisibilityBox02 from "@/components/VisibilityBox02.vue";
 
 export default defineComponent({
   name: "App",
   components: {
+    VisibilityBox02,
     VisibilityBox01,
     DescriptionBox02,
     DescriptionBox01,
---
+++ src/components/VisibilityBox02.vue
@@ -0,0 +1,42 @@
+<template>
+  <EventContainer>
+    <MainForm>
+      <p>Toggle square visibility on checkbox click.</p>
+      <template #second-row>
+        <CustomCheckbox
+          :is-checked="squareVisibility"
+          :onchange="toggleVisibility"
+        />
+      </template>
+    </MainForm>
+    <AdjustableSquare v-if="squareVisibility" />
+  </EventContainer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import EventContainer from "@/components/EventContainer/EventContainer.vue";
+import MainForm from "@/components/MainForm/MainForm.vue";
+import AdjustableSquare from "@/components/AdjustableSquare/AdjustableSquare.vue";
+import CustomCheckbox from "@/components/CustomCheckbox/CustomCheckbox.vue";
+
+export default defineComponent({
+  name: "VisibilityBox02",
+  components: {
+    CustomCheckbox,
+    MainForm,
+    EventContainer,
+    AdjustableSquare,
+  },
+  data: () => ({
+    squareVisibility: true,
+  }),
+  methods: {
+    toggleVisibility() {
+      this.squareVisibility = !this.squareVisibility;
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss" />
