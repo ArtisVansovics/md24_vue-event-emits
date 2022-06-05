d1bf9f690168e452f1abc5665e00131c60a1de37
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-06 00:16:55 +0300

    Created TextSquare, in which to display text

---
+++ src/components/TextSquare/TextSquare.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="square">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TextSquare",
+});
+</script>
+
+<style scoped lang="scss" src="./TextSquare.scss" />
