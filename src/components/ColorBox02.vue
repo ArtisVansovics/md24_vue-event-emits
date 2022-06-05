02de4e6615ef5b881daeccf696ee04285af6f279
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-05 23:18:36 +0300

    Created ColorBox02, in which squares background color automatically changes based on user input

---
+++ src/components/ColorBox02.vue
@@ -0,0 +1,36 @@
+<template>
+  <EventContainer>
+    <MainForm>
+      <TextInput placeholder-text="Change color..." v-model="inputValue" />
+    </MainForm>
+    <AdjustableSquare :current-color="updatedColor" />
+  </EventContainer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import EventContainer from "@/components/EventContainer/EventContainer.vue";
+import TextInput from "@/components/TextInput/TextInput.vue";
+import MainForm from "@/components/MainForm/MainForm.vue";
+import AdjustableSquare from "@/components/AdjustableSquare/AdjustableSquare.vue";
+
+export default defineComponent({
+  name: "ColorBox02",
+  components: {
+    TextInput,
+    MainForm,
+    EventContainer,
+    AdjustableSquare,
+  },
+  data: () => ({
+    inputValue: "",
+  }),
+  computed: {
+    updatedColor() {
+      return this.inputValue;
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss" />
