754236828daac178e173a0f1a4919d3672b5dda5
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-05 23:02:16 +0300

    Created AdjustableSquare component with adjustable background color

---
+++ src/components/AdjustableSquare/AdjustableSquare.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="square" :style="{ backgroundColor: currentColor }"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AdjustableSquare",
+  props: ["currentColor"],
+});
+</script>
+
+<style scoped lang="scss" src="./AdjustableSquare.scss" />
